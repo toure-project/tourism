45f9f5c0c441b07475105640063cb099b91e9f41
Hayat Abdulaziz <hayatabdulaziz48@gmail.com>
2024-03-03 16:46:32 +0400

    i created hotel back end and links

--- tourpro/tourapp/models.py
+++ tourpro/tourapp/models.py
@@ -1,18 +1,18 @@
 from django.db import models
 
-class reat(models.Model):
-  title = models.CharField(max_length=120)
-
-  def __str__(self):
-    return self.title
-
 
 class hotels(models.Model):
-  H_Name =models.CharField( 'hotel name', max_length=120)
-  level = models.ForeignKey(reat,blank= True, null=True, on_delete=models.CASCADE)
-  H_Locaation =models.CharField( 'hotel addres', max_length=120)
-  Discreption = models.TextField(max_length=200)
-  Payment =models.CharField(max_length =120)
-  #photo =
+  H_Name =models.CharField( 'hotel name', max_length=120, default='3')
+  Email =models.CharField( 'hotel name', max_length=120, default='3')
+  Phon_Number =models.CharField( 'hotel name', max_length=120, default='3')
+  H_Locaation =models.CharField( 'hotel addres', max_length=120, default='3')
+  Superior_Room= models.CharField(max_length=30, default='3')
+  Deluxe_Room= models.CharField(max_length=30, default='3')
+  single_Occupation= models.CharField(max_length=30, default='3')
+  Double_Occupation= models.CharField(max_length=30, default='3')  
+  Photo = models.ImageField(null=True, blank=True, upload_to="images/")
 def __str__(self):
-    return self.name
+    return self.H_Name
+
+
+  
--- tourpro/tourapp/views.py
+++ tourpro/tourapp/views.py
@@ -1,41 +1,61 @@
-from django.shortcuts import render, redirect
-from  django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import hotels
 from .forms import hotelform
-from .forms import hotels
+
+
 
 def home(request):
-    return render (request, 'tourapp/index.html')
-
-def login(request):
-    return render (request, 'tourapp/login.html')
+     return render(request, 'tourapp/index.html')
 
 
 def hotel_list(request):
     context = { "hotel_list" :hotels.objects.all() }
     return render(request, 'tourapp/hotel_list.html', context)
 
-
-
-def hotel_form(request, id=0):
-    if request.method == "GET":
-        if id == 0:
-            form = hotelform()
-        else:
-            form = hotelform(instance=hotels.objects.get(pk=id))
-        return render(request, 'tourapp/hotel_form.html', {'form': form})
-    else:
-        if id == 0:
-            form = hotelform(request.POST)
-        else:
-            form = hotelform(request.POST, instance=hotels.objects.get(pk=id))
-
-        if form.is_valid():
-            form.save()
+def register_hotel(request):
+    if request.method == 'POST':
+        H_Name = request.POST.get('hname')
+        Email = request.POST.get('email')
+        Phon_Number = request.POST.get('phone')    
+        H_Locaation = request.POST.get('address')
+        Superior_Room = request.POST.get('sroom')
+        Deluxe_Room = request.POST.get('droom')
+        single_Occupation = request.POST.get('soccopation')
+        Double_Occupation = request.POST.get('doccopatin')
+        Photo = request.FILES['photo']
+        
+        new_hotel = hotels(
+            H_Name=H_Name,
+            Email=Email,
+            Phon_Number=Phon_Number,
+            H_Locaation=H_Locaation,
+            Superior_Room=Superior_Room,
+            Deluxe_Room=Deluxe_Room,
+            single_Occupation=single_Occupation,
+            Double_Occupation=Double_Occupation,
+            Photo=Photo
+        )
+        new_hotel.save()
         return redirect('hotel_list')
-    
+    return render(request, 'tourapp/hotelregistration.html')
 
 
-def hotel_delete(request, id):
-    hotels.objects.get(pk=id).delete()
+def delete_hotel(request, hotel_id):
+    hotel_to_delete = get_object_or_404(hotels, id=hotel_id)
+    hotel_to_delete.delete()
     return redirect('hotel_list')
 
+
+def update_hotel(request, hotel_id):
+    hotel_instance = get_object_or_404(hotels, pk=hotel_id)
+    if request.method == "POST":
+        form = hotelform(request.POST, request.FILES, instance=hotel_instance)
+        if form.is_valid():
+            form.save()
+            return redirect('hotel_list')
+    else:
+        form = hotelform(instance=hotel_instance)
+        return redirect('hotel_list')
+
+    return render(request, 'tourapp/hotelregistration.html', {'form': form})
